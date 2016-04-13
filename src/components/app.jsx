5a87cf75def3ca55166e9f89f4808cd24a46fc7f
mwarnsley <marcus.warnsley@gmail.com>
2016-04-12 20:48:02 -0400

    Adding more files structure and setting up the files for importing and exporting

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,14 @@
-ECHO is on.
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				APP
+			</div>
+		)
+	}
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-ECHO is on.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
