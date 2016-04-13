6696fe53ce04da09b76355c9ddd2f678ba3531a2
mwarnsley <marcus.warnsley@gmail.com>
2016-04-12 20:58:51 -0400

    Setting the state data to use for the quiz

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,92 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			questions: [
+				{
+					id: 1,
+					text: 'What is 2 + 2?',
+					choices: [
+						{
+							id: 'a',
+							text: '1'
+						},
+						{
+							id: 'b',
+							text: '4'
+						},
+						{
+							id: 'c',
+							text: '3'
+						}
+					],
+					correct: 'b'
+				},
+				{
+					id: 2,
+					text: 'Is Javascript the same as Java?',
+					choices: [
+						{
+							id: 'a',
+							text: 'Yes'
+						},
+						{
+							id: 'b',
+							text: "I don't know"
+						},
+						{
+							id: 'c',
+							text: 'No'
+						}
+					],
+					correct: 'c'
+				},
+				{
+					id: 3,
+					text: 'Does Javascript run on the client side?',
+					choices: [
+						{
+							id: 'a',
+							text: 'Yes'
+						},
+						{
+							id: 'b',
+							text: 'No'
+						},
+						{
+							id: 'c',
+							text: 'Not sure'
+						}
+					],
+					correct: 'a'
+				},
+				{
+					id: 1,
+					text: 'Is Javascript a scripting or compiled language?',
+					choices: [
+						{
+							id: 'a',
+							text: "I don't know"
+						},
+						{
+							id: 'b',
+							text: 'Scripting'
+						},
+						{
+							id: 'c',
+							text: 'Compiled'
+						}
+					],
+					correct: 'b'
+				},
+			],
+			score: 0,
+			current: 1
+		}
+	}
+	
 	render(){
 		return(
 			<div>
